ebe15bbf599b1c588621eee2bb49d60f9819cc29
Lebede Ngartera <119684856+Teraces12@users.noreply.github.com>
2025-06-17 20:29:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from dotenv import load_dotenv
+import streamlit as st
+import requests
 import os
+from dotenv import load_dotenv
+
 load_dotenv()
-backend_url = os.getenv("BACKEND_URL", "http://localhost:8000")
+backend_url = os.getenv("BACKEND_URL", "https://ai-catalyst.onrender.com")
